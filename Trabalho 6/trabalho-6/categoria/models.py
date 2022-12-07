1e98f36415f490825af85ebbcf303f5005ac97d7
thiagolop1 <thiago_nascimento@id.uff.br>
2022-12-07 15:12:50 -0300

    Inicialização Trabalho 6

---
+++ Trabalho 6/trabalho-6/categoria/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=70, db_index=True, unique=True)
+
+    class Meta:
+        db_table='categoria'
+        ordering = ('nome',)
+
+    def __str__(self):
+        return self.nome
---
+++ Trabalho 6/trabalho-6/produto/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+from categoria.models import Categoria
+
+
+class Produto(models.Model):
+    categoria = models.ForeignKey(Categoria, related_name='produtos', on_delete=models.DO_NOTHING)
+    nome = models.CharField(max_length=100, db_index=True, unique=True)
+    qtd_estoque = models.IntegerField()
+    preco = models.DecimalField(max_digits=5, decimal_places=2)
+
+    class Meta:
+        db_table='produto'
+
+    def __str__(self):
+        return self.nome
